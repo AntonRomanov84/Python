88997d8020631a708c649f07b838c62bc92f1320
AntonRomanov84 <sunglaseseye@gmail.com>
2021-04-02 16:56:47 +0300

    Задание 1

---
+++ zadacha1.py
@@ -0,0 +1,17 @@
+a = 124
+b = 'Shalom'
+c = 3.14
+d = False
+print('a = ', a, type (a))
+print('b = ', b, type (b))
+print('c = ', c, type (c))
+print('d = ', d, type (d))
+
+num1 = int(input('Введите первое число - '))
+num2 = float(input('Введите второе число - '))
+name1 = input('Введите Ваше имя - ')
+name2 = input('Введите Вашу фамилию - ')
+print('Первое число = ', num1, 'тип:', type(num1))
+print('Второе число = ', num2, 'тип:', type(num2))
+print('Имя = ', name1, 'тип:', type(name1))
+print('Фамилия = ', name2, 'тип:', type(name2))
